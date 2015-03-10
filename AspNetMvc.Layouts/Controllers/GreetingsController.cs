f5281253a0892e2b2688facd54e291e240ec2900
William Willing <william@williamwilling.com>
2015-03-10 15:59:01 +0100

    Created a controller with accompanying views.

---
+++ AspNetMvc.Layouts/Controllers/GreetingsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvc.Layouts
+{
+    public class GreetingsController : Controller
+    {
+        public ActionResult Hello()
+        {
+            return View();
+        }
+
+        public ActionResult Goodbye()
+        {
+            return View();
+        }
+    }
+}
